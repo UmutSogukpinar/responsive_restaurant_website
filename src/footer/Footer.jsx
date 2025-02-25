d825bc03e1179f81ea78243905b23c299cd40f1f
umut_sogukpinar <usogukpinar0@gmail.com>
2025-02-25 20:27:26 +0300

    footer completed

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -11,37 +11,76 @@ const Footer = () => {
           free delivery time.
         </p>
         <ul id="fo-icon-list">
-          <li className="fo-icon-el"></li>
-          <li className="fo-icon-el"></li>
-          <li className="fo-icon-el"></li>
+          <li className="fo-icon-el">
+            <a href="">
+              <i class="ri-facebook-fill"></i>
+            </a>
+          </li>
+          <li className="fo-icon-el">
+            <a href="">
+              <i class="ri-instagram-fill"></i>
+            </a>
+          </li>
+          <li className="fo-icon-el">
+            <a href="">
+              <i class="ri-twitter-x-fill"></i>
+            </a>
+          </li>
         </ul>
       </div>
       <div className="fo-sub">
         <h3>About Us</h3>
         <ul id="fo-link-list">
-          <li className="fo-link-el">About Us</li>
-          <li className="fo-link-el">Features</li>
-          <li className="fo-link-el">News</li>
-          <li className="fo-link-el">Menu</li>
+          <li className="fo-link-el">
+            <a href="">About Us</a>
+          </li>
+          <li className="fo-link-el">
+            <a href="">Features</a>
+          </li>
+          <li className="fo-link-el">
+            <a href="">News</a>
+          </li>
+          <li className="fo-link-el">
+            <a href="">Menu</a>
+          </li>
         </ul>
       </div>
       <div className="fo-sub">
         <h3>Company</h3>
         <ul id="fo-link-list">
-          <li className="fo-link-el">My Food in</li>
-          <li className="fo-link-el">Partner</li>
-          <li className="fo-link-el">FAQ</li>
-          <li className="fo-link-el">Blog</li>
+          <li className="fo-link-el">
+            <a href="">My Food in</a>
+          </li>
+          <li className="fo-link-el">
+            <a href="">Partner</a>
+          </li>
+          <li className="fo-link-el">
+            <a href="">FAQ</a>
+          </li>
+          <li className="fo-link-el">
+            <a href="">Blog</a>
+          </li>
         </ul>
       </div>
+
       <div className="fo-sub">
         <h3>Support</h3>
         <ul id="fo-link-list">
-          <li className="fo-link-el">Account</li>
-          <li className="fo-link-el">Support Center</li>
-          <li className="fo-link-el">Feedback</li>
-          <li className="fo-link-el">Contact Us</li>
-          <li className="fo-link-el">Accessibility</li>
+          <li className="fo-link-el">
+            <a href="">Account</a>
+          </li>
+          <li className="fo-link-el">
+            <a href="">Support Center</a>
+          </li>
+          <li className="fo-link-el">
+            <a href="">Feedback</a>
+          </li>
+          <li className="fo-link-el">
+            <a href="">Contact Us</a>
+          </li>
+          <li className="fo-link-el">
+            <a href="">Accessibility</a>
+          </li>
         </ul>
       </div>
     </div>
