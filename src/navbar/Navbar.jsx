a9e2da6d13a5ba64d665b1bc2ddf2d9df6a18413
umut_sogukpinar <usogukpinar0@gmail.com>
2025-02-26 00:16:33 +0300

    responsive navbar init

--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -1,19 +1,55 @@
-import React from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import "./navbar.css";
-import logo from "../../public/img/log.png"
+import logo from "../../public/img/log.png";
 
 const Navbar = () => {
+  const [scrolled, setScrolled] = useState(false);
+  const [isSmall, setIsSmall] = useState(false);
+
+  const handleScroll = useCallback(() => {
+    setScrolled(window.scrollY > 10);
+  }, []);
+  
+  const handleSize = useCallback(() => {
+    setIsSmall(window.innerWidth < 850);
+  }, []);
+
+  useEffect(() => {
+    handleSize();
+    handleScroll();
+    window.addEventListener("scroll", handleScroll);
+    window.addEventListener("resize", handleSize);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+      window.removeEventListener("resize", handleSize);
+    };
+  }, []);
+
   return (
-    <div id="navbar">
-      <img src={logo} alt="" />
-      <h2>Food In</h2>
-      <ul id="navbar-list">
-        <li className="navbar-list-el"><a href="">Home</a></li>
-        <li className="navbar-list-el"><a href="">Service</a></li>
-        <li className="navbar-list-el"><a href="">About</a></li>
-        <li className="navbar-list-el"><a href="">Contact</a></li>
-      </ul>
-      <button id="explore-button">Explore</button>
+    <div id="navbar" className={scrolled ? "scrolled" : ""}>
+      <div id="navbar-left">
+        <img src={logo} alt="" />
+        <h2>Food In</h2>
+      </div>
+      <div id="navbar-right" className={isSmall ? "small-navbar" : "big-navbar"}>
+        <ul id="navbar-list">
+          <li className="navbar-list-el">
+            <a href="">Home</a>
+          </li>
+          <li className="navbar-list-el">
+            <a href="">Service</a>
+          </li>
+          <li className="navbar-list-el">
+            <a href="">About</a>
+          </li>
+          <li className="navbar-list-el">
+            <a href="">Contact</a>
+          </li>
+        </ul>
+        <button id="explore-button">Explore</button>
+      </div>
+      <div id="navbar-right-s" className={isSmall ? "small-navbar" : "big-navbar"}></div>
     </div>
   );
 };
