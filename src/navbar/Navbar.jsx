00c2fc9b53b43035f817eb41c90fbeab27169065
umut_sogukpinar <usogukpinar0@gmail.com>
2025-02-28 22:51:36 +0300

    responsive navbar added

--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -5,14 +5,20 @@ import logo from "../../public/img/log.png";
 const Navbar = () => {
   const [scrolled, setScrolled] = useState(false);
   const [isSmall, setIsSmall] = useState(false);
+  const [isClicked, setIsClicked] = useState(false);
 
   const handleScroll = useCallback(() => {
-    setScrolled(window.scrollY > 10);
+    setScrolled(window.scrollY > 50);
   }, []);
-  
+
   const handleSize = useCallback(() => {
     setIsSmall(window.innerWidth < 850);
   }, []);
+
+  const handleClicked = () => {
+    if (isClicked) setIsClicked(false);
+    else setIsClicked(true);
+  };
 
   useEffect(() => {
     handleSize();
@@ -32,8 +38,11 @@ const Navbar = () => {
         <img src={logo} alt="" />
         <h2>Food In</h2>
       </div>
-      <div id="navbar-right" className={isSmall ? "small-navbar" : "big-navbar"}>
-        <ul id="navbar-list">
+      <div
+        id="navbar-right"
+        className={isSmall ? "small-navbar" : "big-navbar"}
+      >
+        <ul className="navbar-list">
           <li className="navbar-list-el">
             <a href="">Home</a>
           </li>
@@ -49,7 +58,38 @@ const Navbar = () => {
         </ul>
         <button id="explore-button">Explore</button>
       </div>
-      <div id="navbar-right-s" className={isSmall ? "small-navbar" : "big-navbar"}></div>
+      <div
+        id="navbar-right-s"
+        className={isSmall ? "small-navbar" : "big-navbar"}
+      >
+        <a onClick={handleClicked}>
+          <i className="ri-menu-line"></i>
+        </a>
+      </div>
+      <div
+        id="search-colon"
+        className={`${isClicked ? "fade-in" : ""} ${
+          isSmall ? "small-navbar" : "big-navbar"
+        }`.trim()}
+      >
+        <ul className="navbar-list">
+          <li className="navbar-list-el">
+            <a href="">Home</a>
+          </li>
+          <li className="navbar-list-el">
+            <a href="">Service</a>
+          </li>
+          <li className="navbar-list-el">
+            <a href="">About</a>
+          </li>
+          <li className="navbar-list-el">
+            <a href="">Contact</a>
+          </li>
+          <li className="navbar-list-el">
+            <button>Explore</button>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
