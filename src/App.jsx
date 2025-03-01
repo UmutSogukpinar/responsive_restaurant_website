a0ad001ca26b2e5400efee7c1e9680f831311e6d
umut_sogukpinar <usogukpinar0@gmail.com>
2025-03-02 00:49:11 +0300

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import React, { useState } from 'react'
-import './App.css'
-import Navbar from './navbar/Navbar'
-import Explore from './explore/Explore'
-import Order from './order/Order'
-import Advertisement from './advertisement/Advertisement'
-import Footer from './footer/Footer'
+import React, { useState } from "react";
+import "./App.css";
+import Navbar from "./navbar/Navbar";
+import Explore from "./explore/Explore";
+import Order from "./order/Order";
+import Advertisement from "./advertisement/Advertisement";
+import Footer from "./footer/Footer";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const ScrollToTopButton = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
 
   return (
     <div id="app">
@@ -16,8 +21,11 @@ function App() {
       <Order />
       <Advertisement />
       <Footer />
+      <a id="up-card" onClick={ScrollToTopButton}>
+        <i class="ri-arrow-up-circle-line"></i>
+      </a>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
